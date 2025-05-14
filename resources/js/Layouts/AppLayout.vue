da1fd141561bcdc59683ebebb4f0b39a444ffad3
Jacob Gardiner <jacobegardiner@gmail.com>
2025-05-14 08:54:38 -0400

    Remove the dashboard route and replace with boards.index (#14)

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -53,19 +53,13 @@ provide('pageDetails', {
             <div class="flex">
               <!-- Logo -->
               <div class="shrink-0 flex items-center">
-                <Link :href="route('dashboard')">
+                <Link :href="route('boards.index')">
                   <ApplicationMark class="block h-9 w-auto" />
                 </Link>
               </div>
 
               <!-- Navigation Links -->
               <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                <NavLink
-                  :href="route('dashboard')"
-                  :active="route().current('dashboard')"
-                >
-                  Dashboard
-                </NavLink>
                 <NavLink
                   :href="route('boards.index')"
                   :active="route().current('boards.index')"
@@ -298,10 +292,10 @@ provide('pageDetails', {
         >
           <div class="pt-2 pb-3 space-y-1">
             <ResponsiveNavLink
-              :href="route('dashboard')"
-              :active="route().current('dashboard')"
+              :href="route('boards.index')"
+              :active="route().current('boards.index')"
             >
-              Dashboard
+              Boards
             </ResponsiveNavLink>
           </div>
 
